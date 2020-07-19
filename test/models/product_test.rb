00efea7a827c23e0e4adc4d04c0a30319c993e81
Danilo Filgueira Mendonça <dfmendonca@gmail.com>
2020-07-19 17:56:03 -0300

    test: use factory bot in product test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -3,13 +3,13 @@ require 'test_helper'
 class ProductTest < ActiveSupport::TestCase
 
   test "should not save product without a name" do
-    product = Product.new(name: "")
+    product = build(:product, name: "")
     assert_not product.save, "Saved the product without a name"
   end
 
   test "should not save product with duplicated name" do
-    Product.create(name: "Product 1")
-    product = Product.new(name: "Product 1")
+    create(:product, name: "Product 1")
+    product = build(:product, name: "Product 1")
     assert_not product.save, "Product saved with duplicated name"
   end
 
