23bb26e72a812cbb58e10886ab9c83d646af0d51
Danilo Filgueira Mendonça <dfmendonca@gmail.com>
2020-07-19 18:32:33 -0300

    test: add factory association between product and category

--- test/factories.rb
+++ test/factories.rb
@@ -1,10 +1,11 @@
 # This will guess the User class
 FactoryBot.define do
-  factory :product do
-    sequence(:name)  {|n| "Product##{n}" }
-  end
-
   factory :category do
     sequence(:name)  {|n| "Category##{n}" }
+  end
+
+  factory :product do
+    sequence(:name)  {|n| "Product##{n}" }
+    association :category
   end
 end
